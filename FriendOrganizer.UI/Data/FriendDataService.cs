03755956101ba22ef33ed347d36e059f582512a2
Philip ryström <prys45@hotmail.com>
2017-10-25 08:14:36 +0200

    Add project files.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from a real database
+            yield return new Friend {FirstName = "Kalle", LastName = "Anka"};
+            yield return new Friend { FirstName = "Knatte", LastName = "Anka" };
+            yield return new Friend { FirstName = "Fnatte", LastName = "Anka" };
+            yield return new Friend { FirstName = "Tjatte", LastName = "Anka" };
+
+        }
+    }
+}

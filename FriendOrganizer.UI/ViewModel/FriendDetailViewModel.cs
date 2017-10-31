bfeb2dcba0811437f59518ec01e67969d4096547
Philip ryström <prys45@hotmail.com>
2017-10-31 16:16:37 +0100

    Module6-förra var 5

---
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
+    {
+        private IFriendDataService _dataService;
+
+        public FriendDetailViewModel(IFriendDataService dataService)
+        {
+            _dataService =
+                dataService;
+        }
+
+        public async Task LoadAsync(int friendId)
+        {
+            Friend = await _dataService.GetByIdAsync(friendId);
+        }
+       private Friend _friend { get; set; }
+
+        public Friend Friend
+        {
+            get { return _friend; }
+            private set
+            {
+                _friend = value;
+                OnPropertyChanged();
+
+            }
+        }
+    }
+}
